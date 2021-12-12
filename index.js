9d7c608339f7e9cdd505594a28c544ba28a092a9
emma0913nyush <91943811+emma0913nyush@users.noreply.github.com>
2021-12-12 21:59:51 +0800

    project3

--- index.js
+++ index.js
@@ -9,20 +9,54 @@ let server = http.createServer(app);
 
 //initialize socket.io
 let io = require('socket.io');
+//const { Client } = require('socket.io/dist/client');
 io = new io.Server(server);
 
+let Playernum =0;
+let name=[];
+
 io.sockets.on('connection', (socket) =>{
+
+    //order of clients
     console.log("We have a new client: " + socket.id);
+    if(Playernum==0){
+        name[0]=socket.id;
+    }else{
+        name[1]=socket.id;
+    }
+
+    Playernum++;
+    console.log("Player number"+ Playernum);
+    if (Playernum==1){
+        socket.send('Draw on the left side!!');
+    } else if(Playernum==2){
+        socket.send('Draw on the right side!!');
+    }
     
     //Listen for this client to disconnect
     socket.on('disconnect', () => {
-        console.log("client left: ", socket.id)
+        console.log("client left: ", socket.id);
+        if(socket.id==name[0]){
+            Playernum=0;
+            delete name[0];
+        }else{
+            Playernum=1;
+            delete name[1];
+        }
+
+
     })
 
-    //on receiving 'mousePosition' EMIT/BROADCAST to other clients
+
+    //on receiving 'mousePosition' EMIT/BROADCAST
     socket.on('mousePosition',(data) =>{
         console.log(data);
         io.sockets.emit('mousePositionServer', data);
+    })
+    //timer EMIT/BROADCAST
+    socket.on('timer',(data)=>{
+        console.log(data);
+        io.sockets.emit('timerServer',data);
     })
 })
 
--- public/app.js
+++ public/app.js
@@ -2,6 +2,9 @@
 let socket = io();
 let SelectC = "pink";
 let s =30;
+let timeleft;
+let count=0;
+let countdown=false;
 
 let fillC ={
     r:0,
@@ -13,78 +16,144 @@ socket.on('connect', () => {
     console.log("connected to the server");
 })
 
+//direction for left or right side
+socket.on('message', function(data){
+    document.getElementById("direction").innerHTML = data;
+    if (data=="Draw on the right side!!"){
+        document.getElementById("direction").style.color = "green";
+    }
+});
+
+//timer
+document.getElementById("gameStart").addEventListener("click", function() {
+    clearArtboard();
+    countdown=true;
+    timeleft = 8;
+    socket.emit('timer', timeleft);
+    
+    var downloadTimer = setInterval(function function1() {
+       document.getElementById("countdown").innerHTML = timeleft +
+          "s";
+ 
+       timeleft -= 1;
+       
+       if (timeleft < 0) {
+          clearInterval(downloadTimer);
+          document.getElementById("countdown").innerHTML = "STOP!!!"
+          countdown=false;
+          document.getElementById("gameStart").innerHTML = "Draw Again"
+       }
+    
+    }, 1000);
+
+ });
+
+
+//Slider
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
-
 slider.oninput = function() {
   output.innerHTML = this.value;
   s = this.value;
 }
 
+//color selection
 function colorSelection(selColor){
     var c = selColor.options[selColor.selectedIndex].text;
     SelectC = c;
 }
 
 function setup() {
-    let cnv =createCanvas(800,500);
+    let cnv =createCanvas(800,350);
     background(230,210,240);
     cnv.parent('canvas-container1');
 
+    //drawing data
     socket.on('mousePositionServer',(data)=>{
         console.log(data);
-        fill(data.c.r,data.c.g,data.c.b);
-        ellipse(data.x,data.y,data.z,data.z);
+            fill(data.c.r,data.c.g,data.c.b);
+            ellipse(data.x,data.y,data.z,data.z);
     })
 
+    //timer data
+    socket.on('timerServer',(data)=>{
+       clearArtboard();
+       countdown=true;
+       var downloadTimer = setInterval(function function1() {
+        document.getElementById("countdown").innerHTML = data +
+           "s";
+  
+        data -= 1;
+        if (data < 0) {
+           clearInterval(downloadTimer);
+           document.getElementById("countdown").innerHTML = "STOP!!!";
+           document.getElementById("gameStart").innerHTML = "Draw Again";
+           countdown=false;
+        }
+     }, 1000);
+    })
+
+    //divide the artboard
     noStroke();
     fill(214,252,230);
     rect(width/2, 0, width/2,height);
+    
 }
+//clear
+function clearArtboard(){
+    clear();
+    background(230,210,240);
+    noStroke();
+    fill(214,252,230);
+    rect(width/2, 0, width/2,height);
+} 
 
-
+//draw
 function mouseDragged(){
 
-    if (SelectC =="pink"){
-        noStroke();
-        fillC.r=255;
-        fillC.g=192;
-        fillC.b=203;
+    if(countdown==true){
+
+        if (SelectC =="pink"){
+            noStroke();
+            fillC.r=255;
+            fillC.g=192;
+            fillC.b=203;
+        }
+
+        else if(SelectC =="blue"){
+            noStroke();
+            fillC.r=171;
+            fillC.g=205;
+            fillC.b=239;
+        }
+
+        else if(SelectC =="orange"){
+            noStroke();
+            fillC.r=255;
+            fillC.g=160;
+            fillC.b=122;
+        }
+
+        else{
+            noStroke();
+            fillC.r=238;
+            fillC.g=232;
+            fillC.b=170;
+        }
+
+        fill(fillC.r, fillC.g,fillC.b);
+        ellipse(mouseX,mouseY,s,s);
+
+        let mousePos = {
+            x: mouseX,
+            y: mouseY,
+            z: s,
+            c: fillC,
+        }
+        socket.emit('mousePosition', mousePos);
     }
 
-    else if(SelectC =="blue"){
-        noStroke();
-        fillC.r=171;
-        fillC.g=205;
-        fillC.b=239;
-    }
-
-    else if(SelectC =="orange"){
-        noStroke();
-        fillC.r=255;
-        fillC.g=160;
-        fillC.b=122;
-    }
-
-    else{
-        noStroke();
-        fillC.r=238;
-        fillC.g=232;
-        fillC.b=170;
-    }
-
-    fill(fillC.r, fillC.g,fillC.b);
-    ellipse(mouseX,mouseY,s,s);
-
-
-    let mousePos = {
-        x: mouseX,
-        y: mouseY,
-        z: s,
-        c: fillC,
-    }
-    socket.emit('mousePosition', mousePos);
 }
 
 
